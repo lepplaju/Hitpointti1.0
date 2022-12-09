74526b646d70008904dafd7dadc4a09e7bc9bc3b
lepplaju <lauri.leppanen2@gmail.com>
2022-12-09 19:12:36 +0200

    Liikkuminen ja hiiren seuraaminen
    Yksinkertainen liikkuminen ja objektin rotaatio hiiren perusteella.

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    private Vector2 movementInput;
+    private float moveSpeed = 5f;
+    [SerializeField] private Rigidbody2D rb;
+
+    void Update()
+    {
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
